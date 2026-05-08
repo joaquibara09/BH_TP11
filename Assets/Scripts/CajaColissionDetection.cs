fb8e5263109c6666f8b0d3977d18c8411fdae25c
BARAK <118564478+joaquibara09@users.noreply.github.com>
2026-05-08 14:32:22 -0300

    Initial commit

---
+++ Assets/Scripts/CajaColissionDetection.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CajaColissionDetection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Colision con " + col.gameObject.name);
+        if(col.gameObject.CompareTag("Player"))
+        {
+            Destroy(col.gameObject);
+        } else if(col.gameObject.CompareTag("Piso"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/CajaMovement.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CajaMovement : MonoBehaviour
+{
+    public float speed = 1;
+    void Update()
+    {
+        transform.Translate(0,-speed * Time.deltaTime,0);
+    }
+}
